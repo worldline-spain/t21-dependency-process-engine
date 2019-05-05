569dfd1768e031778b21cf784f153670b73b4f7b
aleksandar9 <aleksandar.nine@gmail.com>
2019-05-05 15:33:23 +0200

    Migrate sample to AndroidX

--- sample/src/main/java/com/worldline/t21dependencysample/MainActivity.java
+++ sample/src/main/java/com/worldline/t21dependencysample/MainActivity.java
@@ -1,10 +1,11 @@
 package com.worldline.t21dependencysample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.worldline.engine.dependency.invoker.Invoker;
 import com.worldline.engine.dependency.listener.ProcessFinishedInterface;
--- sample/src/main/java/com/worldline/t21dependencysample/TimelineItemViewHolder.java
+++ sample/src/main/java/com/worldline/t21dependencysample/TimelineItemViewHolder.java
@@ -1,9 +1,10 @@
 package com.worldline.t21dependencysample;
 
 import android.graphics.Color;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.alorma.timeline.TimelineView;
 
