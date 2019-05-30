a9395ec60260fb8700f5e7d970cd0d2dc559e66f
aleksandar9 <aleksandar.nine@gmail.com>
2019-05-30 20:18:12 +0200

    Change class visibility

--- t21dependencyprocessengine/src/main/java/com/worldline/engine/dependency/command/Command.java
+++ t21dependencyprocessengine/src/main/java/com/worldline/engine/dependency/command/Command.java
@@ -3,7 +3,7 @@ package com.worldline.engine.dependency.command;
 /**
  * Interface definition for a callback to be invoked when the command (task) is executed.
  */
-public interface Command {
+interface Command {
 
     /**
      * Callback method to be invoked when the command starts executing.
